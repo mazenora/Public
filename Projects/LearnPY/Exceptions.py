e962483b597254df40ad56f7cb10f1fb33d1b8e1
ora <mazenorabi@gmail.com>
2019-03-27 18:30:15 +0100

    Version 1.1

--- Projects/LearnPY/Exceptions.py
+++ Projects/LearnPY/Exceptions.py
@@ -27,3 +27,27 @@ while True:
         print(ex)
     else:
         break
+
+while True:
+    try:
+        first_num = int(input("Enter First value: "))
+        second_num = int(input("Enter First value: "))
+        res = first_num / second_num
+        print("result=", res)
+
+    except:
+        # for any exception but will not know which exception is that
+        print("No Way to Continue")
+    else:
+        break
+
+while True:
+    try:
+        username = input("Your Name: ")
+        if len(username) < 5 or username.count(" ")>0:
+            raise ValueError
+    except ValueError:
+        print("Please try Again")
+    else:
+        print("Alles OK")
+        break
